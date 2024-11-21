62adb3cdd7626d6b465239e1f166021c36ee1fd3
aleks <grigorenko@hpe.com>
2024-11-21 13:46:40 +0200

    upd script

--- user_data.sh
+++ user_data.sh
@@ -2,6 +2,5 @@
 apt-get update
 apt-get install -y curl
 MASTER_IP="${master_ip}"
-SSH_USER="ubuntu"
-TOKEN=$(ssh -o StrictHostKeyChecking=no ${SSH_USER}@${MASTER_IP} -t "sudo cat /var/lib/rancher/k3s/server/node-token")
+TOKEN=$(ssh -o StrictHostKeyChecking=no ubuntu@${MASTER_IP} -t "sudo cat /var/lib/rancher/k3s/server/node-token")
 curl -sfL https://get.k3s.io | K3S_URL=https://$MASTER_IP:6443 K3S_TOKEN=$TOKEN sh -
