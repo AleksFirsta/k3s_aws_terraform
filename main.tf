ddc8c1210e74ae8aadf8e24c87a5993c5fcb05ab
aleks <grigorenko@hpe.com>
2024-11-21 11:53:10 +0200

    upd avalability zones

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "ap-southeast-1a"
+  availability_zone       = "${var.aws_region}a"
   map_public_ip_on_launch = true
 
   tags = {
@@ -23,7 +23,7 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "ap-southeast-1b"
+  availability_zone = "${var.aws_region}b"
 
   tags = {
     Name = "Private Subnet"
