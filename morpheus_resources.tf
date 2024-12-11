08adc91033ceaead17534aa1fd4d02a9ca20bb61
Aleks_Grigorenko <grigorenko@hpe.com>
2024-12-11 08:44:00 +0000

    upd

--- morpheus_resources.tf
+++ morpheus_resources.tf
@@ -3,6 +3,13 @@ resource "morpheus_ansible_playbook_task" "k3sconfig" {
   code            = "configk3s"
   ansible_repo_id = data.morpheus_integration.ansible_repo.id
   playbook        = "k3sconfig"
-  command_options = "-o ProxyJump=ubuntu@${aws_instance.k3s_master.public_ip}"
-  execute_target  = "server"
+  command_options = "-v"
+  execute_target  = "local"
+  depends_on      = [aws_instance.nginx]
 }
+
+# пример без использования инвентаря
+# ansible-playbook -i "web1,web2," playbook.yml \
+#   --user remoteuser \
+#   --private-key ~/.ssh/remote_key \
+#   --ssh-common-args='-o ProxyJump=jumpuser@192.168.1.100'
